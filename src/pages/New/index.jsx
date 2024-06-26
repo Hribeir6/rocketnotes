1dd2496f2aa05887d86b07ce324f41193bd9c5f1
Henrique <hribeir6@gmail.com>
2024-06-25 23:19:42 -0300

    add Interface New to page

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,14 @@
+import { Header } from "../../components/Header"
+
+import { Container } from "./style";
+
+export function New(){ 
+  return(
+
+    <Container> 
+      <Header /> 
+
+    </Container>
+
+  );
+}
